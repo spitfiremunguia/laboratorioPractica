b0265541be826c6c65ab0823e0ab79d6f605584e
SpitfireMunguia <davidmunguiaamericas@gmail.com>
2017-09-11 17:58:56 -0600

    Arreglado

--- laboratorioPractica/Form1.cs
+++ laboratorioPractica/Form1.cs
@@ -7,9 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Id3Lib;
-using ICSharpCode;
-using Mp3Lib;
 using System.IO;
 using laboratorioPractica.Properties;
 
